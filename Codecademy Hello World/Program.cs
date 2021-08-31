8652d4863691e498f008d6e63979713d59604c87
Thomas-474 <teg2004@icloud.com>
2021-08-31 09:06:57 -0400

    Add project files.

---
+++ Codecademy Hello World/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecademy_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thomas");
+
+            // this is a comment
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+
+            Console.ReadKey();
+        }
+    }
+}
